fd1a0f953e78f4a0885b852cd8deb336b412c9e2
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2024-01-04 19:32:09 +0530

    done

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,16 +1,16 @@
-import React, { useRef, useState } from 'react';
-import '../Styles/note.css'
-const Note = ({ id, text,  onDelete, onEdit, onPin, isPinned  }) => {
+import React, { useRef, useState, useEffect } from 'react';
+import '../Styles/note.css';
+
+const Note = ({ id, text, onDelete, onEdit, onPin, isPinned }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [noteText, setNoteText] = useState(text);
-  const [Move, setMove] = useState(false);
+  const [isMoving, setIsMoving] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   const stickyNoteRef = useRef();
 
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
   const handleDelete = () => {
-    onDelete(id)  };
+    onDelete(id);
+  };
 
   const handleDoubleClick = () => {
     setIsEditing(true);
@@ -23,32 +23,41 @@ const Note = ({ id, text,  onDelete, onEdit, onPin, isPinned  }) => {
 
   const handlePin = () => {
     onPin(id);
-    isPinned=!isPinned
-    setMove(isPinned)
   };
-   
-  function handleMouseDown(e) {
-    console.log(e.nativeEvent.screenX)
-    setMove(true);
+
+  const handleMouseDown = (e) => {
+    setIsMoving(true);
     const dimensions = stickyNoteRef.current.getBoundingClientRect();
-    setX(e.nativeEvent.screenX - dimensions.x);
-    setY(e.nativeEvent.screenY - dimensions.y);
-  }
+    setPosition({
+      x: e.clientX - dimensions.left,
+      y: e.clientY - dimensions.top,
+    });
+  };
 
-  function handleMouseMove(e) {
+  const handleMouseMove = (e) => {
+    if (isMoving) {
+      const x_axis = e.clientX - position.x;
+      const y_axis = e.clientY - position.y;
 
-    if (Move) {
-      const x_axis = e.nativeEvent.screenX - x;
-      const y_axis = e.nativeEvent.screenY - y;
-      
-      stickyNoteRef.current.style.left = x_axis + "px";
-      stickyNoteRef.current.style.top = y_axis + "px";
+      stickyNoteRef.current.style.left = x_axis + 'px';
+      stickyNoteRef.current.style.top = y_axis + 'px';
     }
-  }
+  };
 
-  function handleMouseUp() {
-    setMove(false);
-  }
+  const handleMouseUp = () => {
+    setIsMoving(false);
+  };
+
+  useEffect(() => {
+    const x_axis = Math.random() * window.innerWidth * 0.7;
+    const y_axis = Math.random() * window.innerHeight * 0.7;
+
+    setPosition({ x: x_axis, y: y_axis });
+
+    stickyNoteRef.current.style.left = x_axis + 'px';
+    stickyNoteRef.current.style.top = y_axis + 'px';
+  }, [id]);
+
   return (
     <div
       className={`note ${isPinned ? 'pinned' : ''}`}
@@ -56,7 +65,6 @@ const Note = ({ id, text,  onDelete, onEdit, onPin, isPinned  }) => {
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-      
     >
       {isEditing ? (
         <textarea
@@ -71,11 +79,18 @@ const Note = ({ id, text,  onDelete, onEdit, onPin, isPinned  }) => {
       ) : (
         <div onDoubleClick={handleDoubleClick} className="note-container">
           <div className="button-container">
-            <button onClick={handlePin} style={{fontSize:"15px"}}>{isPinned ? 'Unpin' : 'Pin'}</button>
+            <button onClick={handlePin} style={{ fontSize: '15px' }}>
+              {isPinned ? 'Unpin' : 'Pin'}
+            </button>
             <span>Text {id}</span>
-            <button onClick={handleDelete} className='clear'><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="20"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg></button>
+            <button onClick={handleDelete} className="clear">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="20">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+              </svg>
+            </button>
           </div>
-          <div className='text'>{text}</div>
+          <div className="text">{text}</div>
         </div>
       )}
     </div>
